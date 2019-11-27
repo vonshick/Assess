842d77653c3682720b7af340dc228bf6104715fe
rochusqwerty <rochus96@gmail.com>
2019-11-27 11:01:00 +0100

    Change Alternative - Add class AlternativeValue

---
+++ DataModel/Input/AlternativeEntry.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DataModel.Input
+{
+    public class AlternativeEntry
+    {
+        public AlternativeEntry()
+        {
+        }
+
+        public AlternativeEntry(string name, List<AlternativeValue> listOfAlternativeValue)
+        {
+            Name = name;
+            ListAlternativeValue = listOfAlternativeValue;
+        }
+
+        public string Name { get; set; }
+        public List<AlternativeValue> ListAlternativeValue { get; set; }
+    }
+}
---
+++ DataModel/Input/AlternativeValue.cs
@@ -0,0 +1,18 @@
+﻿namespace DataModel.Input
+{
+    public class AlternativeValue
+    {
+        public AlternativeValue()
+        {
+        }
+
+        public AlternativeValue(Criterion criterion, float value)
+        {
+            CriterionObj = criterion;
+            Value = value;
+        }
+
+        public Criterion CriterionObj { get; set; }
+        public float Value { get; set; }
+    }
+}
