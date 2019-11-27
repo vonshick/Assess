9488a70471f3416f1c3188effc58eea0233aaf95
rochusqwerty <rochus96@gmail.com>
2019-11-27 11:02:21 +0100

    Add rank to Ranking

--- DataModel/Results/Ranking.cs
+++ DataModel/Results/Ranking.cs
@@ -5,11 +5,20 @@ namespace DataModel.Results
 {
     public class Ranking
     {
-        public Ranking(List<Alternative> variantsList)
+        public Ranking()
+        {
+            VariantsList = new List<KeyValuePair<AlternativeEntry, int>>();
+        }
+        public Ranking(List<KeyValuePair<AlternativeEntry, int>> variantsList)
         {
             VariantsList = variantsList;
         }
+
+        public void addVariant(AlternativeEntry alternative, int rank)
+        {
+            VariantsList.Add(new KeyValuePair<AlternativeEntry, int>(alternative, rank));
+        }
         /// <summary> order of variants in list defines reference ranking order </summary>
-        public List<Alternative> VariantsList { get; set; }
+        public List<KeyValuePair<AlternativeEntry, int>> VariantsList { get; set; }
     }
 }
