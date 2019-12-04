c6a1e351464255b8442d1f0d6e86f5e0bfa432c3
rochusqwerty <rochus96@gmail.com>
2019-12-04 02:11:30 +0100

    Add Dict(point, value) to PartialUtilityFunction

--- DataModel/Results/PartialUtilityFunction.cs
+++ DataModel/Results/PartialUtilityFunction.cs
@@ -1,7 +1,15 @@
-﻿namespace DataModel.Results
+﻿using DataModel.Input;
+using System.Collections.Generic;
+
+namespace DataModel.Results
 {
     public class PartialUtilityFunction
     {
+        public Criterion Criterion { get; set; }
+
+        //Dict(point, value) - point from linear segment
+        public Dictionary<float, float> PointsValues { get; set; }
+
         public PartialUtilityFunction()
         {
         }
