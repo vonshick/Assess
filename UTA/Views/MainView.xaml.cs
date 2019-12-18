147e98da41bba42a62f6a2b5a2316858d2a669a8
vonshick <kubawasik@vp.pl>
2019-12-18 15:10:09 +0100

    Test import and export

--- UTA/Views/MainView.xaml.cs
+++ UTA/Views/MainView.xaml.cs
@@ -26,13 +26,10 @@ namespace UTA.Views
 
       public MainView()
       {
-         // TODO vonshick REMOVE IT AFTER TESTING
-         // DataLoader csvLoader = SampleImport.ProcessSampleData(true, false, false); // csv
-         // DataLoader utxLoader = SampleImport.ProcessSampleData(false, true, false); // utx
-         // DataLoader xmlLoader = SampleImport.ProcessSampleData(false, false, true); // xml
-         // TODO vonshick REMOVE IT AFTER TESTING
-         // XMCDALoader xmcdaLoader = SampleImport.ProcessXMCDA(); // xmcda            
-         // SampleExport.exportXMCDA(xmcdaLoader.CriterionList, xmcdaLoader.AlternativeList);
+//         TODO vonshick REMOVE IT AFTER TESTING
+//         string dataDirectoryPath = "C:\\Data";
+//         DataLoader dataLoader = SampleImport.ProcessSampleData(dataDirectoryPath); // csv
+//         SampleExport.exportXMCDA(dataDirectoryPath, dataLoader.CriterionList, dataLoader.AlternativeList);
 
          InitializeComponent();
          DataContext = _viewmodel;
